ddf88134e9548dbc311b6932a371b79212ec8160
ShubhamB2595 <shubhambhele95@gmail.com>
2022-02-04 22:19:19 +0530

    Code Refactored with failed test and ss

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,15 +2,22 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 
 @Listeners(CustomListeners.class)
 public class TestClass extends BaseClass {
 
     public String HOMEPAGE_URL = "https://www.bookswagon.com/";
 
-    @Test
+
+    @Test(priority = 0)
     public void launchingHomepage() {
         driver.get(HOMEPAGE_URL);
         assertEquals(driver.getCurrentUrl(), HOMEPAGE_URL);
+    }
+
+    @Test(priority = 1)
+    public void failedCase() {
+        assertFalse(true);
     }
 }
