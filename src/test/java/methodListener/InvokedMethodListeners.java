71251aaf69731a4ad277aedb34d1047a5ccf2917
ShubhamB2595 <shubhambhele95@gmail.com>
2022-02-05 19:06:26 +0530

    code ADDED for the IInvokedMethod listener

---
+++ src/test/java/methodListener/InvokedMethodListeners.java
@@ -0,0 +1,18 @@
+package methodListener;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+public class InvokedMethodListeners implements IInvokedMethodListener {
+
+    @Override
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        System.out.println("Before Invocation of: " + method.getTestMethod().getMethodName() + "of Class:" + testResult.getTestClass());
+    }
+
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        System.out.println("After Invocation of: " + method.getTestMethod().getMethodName() + "of Class:" + testResult.getTestClass());
+    }
+}
